9bf756d8fa985793fd3e7f6b06329d8394d182bd
Bikram chakraborty <bikramcharaborty09123@gamil.com>
2024-08-28 23:58:45 +0530

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+const id = 987;
+let age = 18;
+var name = "Bikram";
+state = "student";
+
+// id=900  // const variable data must not be change
+
+/*
+var keyword should not be use due to block scope
+*/
+
+console.log(id)
+console.log(name);
+console.table([id,age,name,state])
+
