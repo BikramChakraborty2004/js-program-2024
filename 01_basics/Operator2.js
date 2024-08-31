25860319d19c29d1e78a9192ac59da6646761377
Bikram chakraborty <bikramchakraborty09123@gmail.com>
2024-08-31 22:41:46 +0530

    operator2

--- 01_basics/Operator2.js
+++ 01_basics/Operator2.js
@@ -1,21 +1,30 @@
-let num = 78
-let strnum = "78"
-console.log(num == strnum);
-console.log(num === strnum);
+// let num = 78;
+// let strnum = "78"
+// // console.log(num == strnum);
+// // console.log(num === strnum);
+
+console.log("2" > 1);
+console.log("02" > 1);
+// The Comparison between two Different data type should not occur
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null <= 0);
 
 // For % operator Sign of the Dividend is considered 
-console.log(-5 % 3)
-console.log(5 % -3)
-console.log(-5 % -3)
+// console.log(-5 % 3)
+// console.log(5 % -3)
+// console.log(-5 % -3)
 
-console.log(-5 / 3)
-console.log(5 / -3)
-console.log(-5 / -3)
+// console.log(-5 / 3)
+// console.log(5 / -3)
+// console.log(-5 / -3)
 
-const x = 4;
-const y = -1;
-console.log(+x);
-console.log(+y);
-console.log(+true);
-console.log(+false);
+// const x = 4;
+// const y = -1;
+// console.log(+x);
+// console.log(+y);
+// console.log(+true);
+// console.log(+false);
 
