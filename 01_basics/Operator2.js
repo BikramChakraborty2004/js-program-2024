01bc1ab25498309eff3020025dd71248f4f772c8
Bikram chakraborty <bikramchakraborty09123@gmail.com>
2024-08-31 22:15:32 +0530

    operator2

---
+++ 01_basics/Operator2.js
@@ -0,0 +1,21 @@
+let num = 78
+let strnum = "78"
+console.log(num == strnum);
+console.log(num === strnum);
+
+// For % operator Sign of the Dividend is considered 
+console.log(-5 % 3)
+console.log(5 % -3)
+console.log(-5 % -3)
+
+console.log(-5 / 3)
+console.log(5 / -3)
+console.log(-5 / -3)
+
+const x = 4;
+const y = -1;
+console.log(+x);
+console.log(+y);
+console.log(+true);
+console.log(+false);
+
