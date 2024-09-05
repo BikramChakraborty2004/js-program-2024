120672ab59cdb7aed75659e64e773990286d1bda
Bikram chakraborty <bikramchakraborty09123@gmail.com>
2024-09-05 22:28:52 +0530

    variable

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -2,7 +2,8 @@ const id = 987;
 let age = 18;
 var name = "Bikram";
 state = "student"
-
+let temperature = null // Thats means Temperature variable is empty ** It does not mean ZERO
+let variable;  // If we not initialise any variable it will be undefined 
 // id=900  // const variable data must not be change
 
 /*
