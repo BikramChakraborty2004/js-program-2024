47a58cbc78d8399324cbb92c4d36b693fa14e6a9
Bikram chakraborty <bikramchakraborty09123@gmail.com>
2024-09-05 23:09:52 +0530

    Maths1

---
+++ 01_basics/Maths1.js
@@ -0,0 +1,25 @@
+const n = -17
+console.log(Math.abs(n));
+
+const n1 = 23.563
+console.log(Math.round(n1));
+console.log(Math.floor(n1));
+console.log(Math.ceil(n1));
+
+const arr = [5,3,5,8,7,2,1]
+console.log(Math.min(...arr));
+console.log(Math.max(...arr));
+ 
+//#####################/* RANDOM */#####################################
+const num = Math.random();
+console.log(num);
+const min = 55 
+const max = 57
+const number = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(number);
+
+
+
+
+
+
