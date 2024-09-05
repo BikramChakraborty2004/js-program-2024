81daac14cb1646d5dd5287b34b148425cbfa5950
Bikram chakraborty <bikramchakraborty09123@gmail.com>
2024-09-05 22:28:41 +0530

    string2

---
+++ 01_basics/String2.js
@@ -0,0 +1,11 @@
+const str = "Bikram-chakraborty-the-boy";
+const Stringstr = str.split('-');  // Split whole string into String type array
+console.log(Stringstr);
+
+const url = "https://www.youtube.com/"
+console.log(url.replace('w','@'));
+console.log(url.replaceAll('w','@'));
+
+console.log(url.includes('com'));
+
+
