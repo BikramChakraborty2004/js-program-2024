5f1d1f8407ed4fb21dc500c1b9d9fec556dc5025
Bikram chakraborty <bikramchakraborty09123@gmail.com>
2024-09-05 22:28:15 +0530

    Number

---
+++ 01_basics/Number.js
@@ -0,0 +1,17 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(12345);
+console.log(balance);
+console.log(balance.toString().length);   // toString converts into string
+
+const n = 123.567
+console.log(n.toFixed(2)); // after decimal how many digit we want to show 
+
+const hundreds = 20000000000
+console.log(hundreds.toLocaleString('en-In'));
+console.log(hundreds.toExponential(2));
+
+
+
+
