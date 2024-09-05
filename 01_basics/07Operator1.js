633fd33035c11b0828d4886ffdafe87faa540132
Bikram chakraborty <bikramchakraborty09123@gmail.com>
2024-09-06 00:48:40 +0530

    operator

---
+++ 01_basics/07Operator1.js
@@ -0,0 +1,33 @@
+// let num = 33
+// let numstr = String(num)   // for type conversion
+// console.log(typeof num);
+// console.log(typeof numstr);
+
+let n = 3
+let neg_n = -n
+console.log(n);
+console.log(neg_n);
+
+// For Some operator
+console.log(2**3);  // For 2 to the power 3
+console.log(5/3);   // Get Quotient 
+console.log(5%3);  // Get Remainder 
+
+// For Concatination
+let str1 = "Hello "
+let str2 = "Bikram"
+let str3 = str1 + str2
+console.log(str3);
+ 
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + "2"); 
+console.log(1 + 2 + "3");
+
+
+
+
+
+ 
+
+
