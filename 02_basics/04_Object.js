88bfe77abeddca7200b4ade20e6205555b409b21
Mr.Bikram <142298115+BikramChakraborty2004@users.noreply.github.com>
2025-08-24 06:16:23 +0000

    obejct part 2

---
+++ 02_basics/04_Object.js
@@ -0,0 +1,22 @@
+const tinderUser2 = {}  // Non Singletone object using literals 
+//console.log(tinderUser2);
+
+const tinderUser = new Object(); // Singletone object (using constructor)
+tinderUser.id = "123abc"
+tinderUser.name = 'sammy'
+//console.log(tinderUser);
+
+const regularUser = {
+    email:"abc@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname : "Bikram",
+            lastname : "Chakraborty"
+        }
+    }
+}
+console.log(regularUser.);
+
+
+
+
