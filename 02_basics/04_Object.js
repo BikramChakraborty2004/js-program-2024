6c2d3e4bed66d68690cecc68f5fa42f4ed95d9ad
Mr Bikram <bikramchakraborty09123@gmail.com>
2025-08-24 12:14:05 +0530

    object part 2

--- 02_basics/04_Object.js
+++ 02_basics/04_Object.js
@@ -6,6 +6,7 @@ tinderUser.id = "123abc"
 tinderUser.name = 'sammy'
 //console.log(tinderUser);
 
+// Nested object
 const regularUser = {
     email:"abc@gmail.com",
     fullname:{
@@ -15,7 +16,56 @@ const regularUser = {
         }
     }
 }
-console.log(regularUser.);
+console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname.userfullname.lastname);
+
+// ** merge two object **
+const obj1 = {1: "a", 2:'b'}
+const obj2 = {3: 'c', 4:'d'}
+
+const obj3 = {obj1, obj2}; 
+// inside obj3, two objects present obj1, obj2
+console.log(obj3);
+
+// if i want that 2 object convert into 1 then
+const obj4 = Object.assign(obj1,obj2); 
+// Recommended syntax-->> Object.assign({},obj1,obj2)
+console.log(obj4);
+// Using spread operator
+const obj5 = {...obj1,...obj2}
+console.log(obj5);
+
+// IMPORTANT CONCEPT (Array of object)
+const users = [
+    {
+        name:"Bikram",
+        age :22
+    },
+    {
+         name:"Raj",
+        age :25
+    },
+    {
+         name:"Ramesh",
+        age :45
+    }
+]
+console.log(users[1].name);
+
+console.log(tinderUser);
+// Object.keys() return an String array of keys
+console.log(Object.keys(tinderUser));
+// Object.values() return an array of values 
+console.log(Object.values(tinderUser)); 
+
+// check whether a property present is not 
+console.log(tinderUser.hasOwnProperty('name'));// True or false 
+ 
+
+
+
+
+
 
 
 
